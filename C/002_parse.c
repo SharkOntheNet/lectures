c2f37614136622900524fc0fe4f3b5b70c962f12
SharkOntheNet <leeswdaddy@gmail.com>
2022-04-10 10:27:22 -0400

    Parse

---
+++ C/002_parse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>	/* printf, sprintf */
+#include <string.h>	/* memset, memcpy, memcmp, strtok_r */
+#include <stdlib.h>	/* atoi */
+
+/*
+ * Written by leewsdaddy@gmail.com for SW LEE
+ * This is about how to parse a string.
+ * You have learned how to handle char and char array.
+ * Now, we're going to use it practically.
+ */
+
+void printArgs (int argc, char **argv){
+}
+
+int main (int argc, char **argv){
+	
+}
