32325efde60e76ac02d35fcfc466f383967bbc9a
sharkonthenet <leeswdaddy@gmail.com>
2022-04-12 14:48:22 +0900

    change

--- C/002_parse.c
+++ C/002_parse.c
@@ -9,9 +9,39 @@
  * Now, we're going to use it practically.
  */
 
+/* I'm thinking about make two files, 002_parseStrtok_r.c 002_parseGetopt.c */
+
+/*
+ * Print out command line arguments
+ */
 void printArgs (int argc, char **argv){
+	int i=0;
+	printf ("argc=>%d\n", argc);
+	for (i=0; i<argc; i++){
+		printf ("argv[%d]=>\"%s\"\n", i, argv[i]);
+	}
+}
+
+/*
+ * Get a string to parse and a delimiter.
+ * Print out parsed tokens(strings).
+ * Using strtok_r in string.h
+ * https://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/s/strtok_r.html
+ */
+int useStrtok_r (char *x){
+	return 1;
+}
+
+/*
+ * Get all the arguments and parse with option flags
+ * Using getopt function in unistd.h
+ * https://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/g/getopt.html
+ */
+int useGetopt (int argc, char **argv){
+	return 1;
 }
 
 int main (int argc, char **argv){
-	
+	printArgs (argc, argv);
+	return 1;
 }
