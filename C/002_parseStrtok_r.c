a633e9b3f1f35b92da18af62897313cded4b73b6
sharkonthenet <leeswdaddy@gmail.com>
2022-04-12 14:50:26 +0900

    strtok getopt added

---
+++ C/002_parseStrtok_r.c
@@ -0,0 +1,47 @@
+#include <stdio.h>	/* printf, sprintf */
+#include <string.h>	/* memset, memcpy, memcmp, strtok_r */
+#include <stdlib.h>	/* atoi */
+
+/*
+ * Written by leewsdaddy@gmail.com for SW LEE
+ * This is about how to parse a string.
+ * You have learned how to handle char and char array.
+ * Now, we're going to use it practically.
+ */
+
+/* I'm thinking about make two files, 002_parseStrtok_r.c 002_parseGetopt.c */
+
+/*
+ * Print out command line arguments
+ */
+void printArgs (int argc, char **argv){
+	int i=0;
+	printf ("argc=>%d\n", argc);
+	for (i=0; i<argc; i++){
+		printf ("argv[%d]=>\"%s\"\n", i, argv[i]);
+	}
+}
+
+/*
+ * Get a string to parse and a delimiter.
+ * Print out parsed tokens(strings).
+ * Using strtok_r in string.h
+ * https://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/s/strtok_r.html
+ */
+int useStrtok_r (char *x){
+	return 1;
+}
+
+/*
+ * Get all the arguments and parse with option flags
+ * Using getopt function in unistd.h
+ * https://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/g/getopt.html
+ */
+int useGetopt (int argc, char **argv){
+	return 1;
+}
+
+int main (int argc, char **argv){
+	printArgs (argc, argv);
+	return 1;
+}
